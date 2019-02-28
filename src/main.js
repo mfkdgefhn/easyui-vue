71b3ec3129876c9f3d702c90d38ccbf3fc8c94b4
mfkdgefhn <mfkdgefhn@163.com>
2019-02-28 10:44:02 +0800

    enter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'vx-easyui/dist/themes/default/easyui.css';
+import 'vx-easyui/dist/themes/icon.css';
+import 'vx-easyui/dist/themes/vue.css';
+import EasyUI from 'vx-easyui';
+Vue.use(EasyUI);
+
 Vue.config.productionTip = false
 
 new Vue({
